c69d84238913981fccc6240d6d35c9c2329002f3
ivan-kaliaskin <jabr.don24@gmail.com>
2022-08-08 22:56:41 +0400

    init: start guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.bottom;
+        this.top;
+    }
 
     setRange(min, max) {
-
+        this.bottom = min;
+        this.top = max
     }
 
     guess() {
-
+        return Math.round((this.top + this.bottom) / 2)
     }
 
     lower() {
-
+        const newTop = Math.round((this.top + this.bottom) / 2)
+        this.top = newTop
     }
 
     greater() {
-
+        const newBottom = Math.round((this.top + this.bottom) / 2)
+        this.bottom = newBottom
     }
 }
-
 module.exports = GuessingGame;
